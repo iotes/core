4e99a5fe70f178054382e8d931661458ff3be65b
Seamus Foley <seamus.foley@icloud.com>
2020-05-12 20:27:01 +1200

    Switched to github

--- dist/utils/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { DeviceDispatchable, HostDispatchable, CreateDeviceDispatchable, CreateHostDispatchable } from '../types';
-export declare const createDeviceDispatchable: CreateDeviceDispatchable;
-export declare const createHostDispatchable: CreateHostDispatchable;
-export declare const insertMetadata: <Payload extends {
-    [key: string]: any;
-}>(dispatchable: HostDispatchable<any> | DeviceDispatchable<Payload>, meta: {
-    [key: string]: string | number;
-}) => {};
